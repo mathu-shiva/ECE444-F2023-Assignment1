b3b105c7db5f1726f19d5662e927e77a16c79be2
Mathura Shivakaran <mathushiva@Mathuras-MacBook-Air.local>
2023-09-13 18:50:03 -0400

    Made some changes to the code

--- utils.py
+++ utils.py
@@ -4,16 +4,23 @@ class Utils:
     def Reversed(number):
         # Converting the number to string
         numAsStr = str(number)
-
-        # Two scenarios when number is a single digit vs. greater digits
-        if number < 10:
-            print("The reverse of the number " + numAsStr + " is " + numAsStr + "0.")
-        else:
-            reversedNumber = numAsStr[::-1]
-            print("The reverse of the number " + numAsStr + " is " + reversedNumber + ".")
+        # Function does not run if input is not an integer
+        if(numAsStr.isdigit() == False):
+            return -1
+        # Reversing the number using slicing
+        reversedNumber = numAsStr[::-1]
+        print("The reverse of the number " + numAsStr + " is " + reversedNumber + ".")
+        # Returning the reversed number back in integer form
+        return (int(reversedNumber))
 
     # Function that converts decimal number into binary and octal
     def Formatter(number):
+        #Function does not run if input is not an integer
+        if(str(number).isdigit() == False):
+            return -1
         print("The value of " + str(number) + " is:")
         print(str(bin(number)) + " in binary.")
         print(str(oct(number)) + " in octal.")
+        # Returning the binary and octal formats of the number
+        return (bin(number), oct(number))
+
